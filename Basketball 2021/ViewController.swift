c130327d88f2218fbd1c68c089c4d0fc244d962a
Evgen-ios <jack38irk@icloud.com>
2021-07-02 18:41:14 +0800

    Added Score places in rootNode

--- Basketball 2021/ViewController.swift
+++ Basketball 2021/ViewController.swift
@@ -10,11 +10,35 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
-    @IBOutlet var sceneView: ARSCNView!
     
+    // MARK: - IBOutlets
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    // MARK: Private Properties
+    var score: Int = 0 {
+        didSet
+        {
+            scoreLabel.text = "СЧЕТ: \(self.score)"
+        }
+    }
+    
+    let configuration  = ARWorldTrackingConfiguration()
+    
+    
+    var isHoopAdded = false {
+        didSet {
+            configuration.planeDetection = []
+            sceneView.session.run(configuration, options: .removeExistingAnchors)
+        }
+    }
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Set debug options
+        sceneView.debugOptions = [.showFeaturePoints]
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -26,9 +50,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
+        // Detect vertical plane
+        configuration.planeDetection = [.vertical, .horizontal]
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -39,30 +63,147 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    // MARK: Private Methods
+    private func getBall() -> SCNNode? {
+        
+        // Get curret frame
+        guard let frame = sceneView.session.currentFrame else { return nil }
+        
+        //Get camera transform
+        let cameraTransform = frame.camera.transform
+        let matrixCameraTransform = SCNMatrix4(cameraTransform)
+        
+        // Ball geometry
+        let ball = SCNSphere(radius: 0.125)
+        ball.firstMaterial?.diffuse.contents = UIImage(named: "basketball")
+        
+        // Ball node
+        let ballNode = SCNNode(geometry: ball)
+        
+        // Add physics body
+        ballNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: ballNode))
+        
+        // Calculate force for pushing the ball
+        let power = Float(10)
+        let x = -matrixCameraTransform.m31 * power
+        let y = -matrixCameraTransform.m32 * power
+        let z = -matrixCameraTransform.m33 * power
+        let forceDirection = SCNVector3(x, y, z)
+        
+        // Apply force
+        ballNode.physicsBody?.applyForce(forceDirection, asImpulse: true)
+        
+    
+        // Assing camera position to ball
+        ballNode.simdTransform = frame.camera.transform
+        
+        return ballNode
+    }
+    
+    private func getHoopNode() -> SCNNode {
+        let scene = SCNScene(named: "Hoop.scn", inDirectory: "art.scnassets")!
+        let hoopNode = scene.rootNode.clone()
+  
+        // Add physics body
+        let childNodes = hoopNode.childNodes
+        
+        for index in childNodes {
+            index.physicsBody = SCNPhysicsBody(
+                type: .static,
+                shape: SCNPhysicsShape(
+                    node: index,
+                    options:[SCNPhysicsShape.Option.type: SCNPhysicsShape.ShapeType.concavePolyhedron]
+                )
+            )
+        }
 
+        // Rotate hoop node to make it vertical
+        hoopNode.eulerAngles.x -= .pi / 2
+        
+        return hoopNode
+    }
+    
+    private func getPlaneNode(for anchor: ARPlaneAnchor) -> SCNNode {
+        let extent = anchor.extent
+        let plane = SCNPlane(width: CGFloat(extent.x), height: CGFloat(extent.z))
+        plane.firstMaterial?.diffuse.contents = UIColor.green
+        
+        // Create 50% transparent plane node
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.5
+        
+        // Rotate plane node
+        planeNode.eulerAngles.x -= .pi / 2
+        
+        return planeNode
+    }
+    
+    private func updatePlaneNode(_ node: SCNNode, for anchor: ARPlaneAnchor) {
+        guard let planeNode = node.childNodes.first, let plane = planeNode.geometry as? SCNPlane else {
+            return
+        }
+        
+        // Chenge plane node center
+        planeNode.simdPosition = anchor.center
+        
+        // Change plane size
+        let extent = anchor.extent
+        plane.width = CGFloat(extent.x)
+        plane.height = CGFloat(extent.z)
+        
+    }
+    
     // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else {
+            return
+        }
         
+        // Add the hoop to the center of detected vertial plane
+        node.addChildNode(getPlaneNode(for: planeAnchor))
+        print(#line, #function, "Vertical plane found! \(planeAnchor)")
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor, planeAnchor.alignment == .vertical else {
+            return
+        }
         
+        // Update plane node
+        updatePlaneNode(node, for: planeAnchor)
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+    // MARK: - IBActions
+    @IBAction func userTapped(_ sender: UITapGestureRecognizer) {
         
+        if isHoopAdded {
+            // Get basketball node
+            guard  let ballNode = getBall() else { return }
+            
+            // Add basketball to the camera position
+            sceneView.scene.rootNode.addChildNode(ballNode)
+            
+        } else {
+            let location = sender.location(in: sceneView)
+            
+            guard let result = sceneView.hitTest(location, types: .existingPlaneUsingExtent).first else {
+                return
+            }
+            
+            guard let anchor = result.anchor as? ARPlaneAnchor, anchor.alignment == .vertical else {
+                return
+            }
+            
+            // Get hoop node and set its coodinates to the point of user touch
+            let hoopeNode = getHoopNode()
+            hoopeNode.simdTransform = result.worldTransform
+            
+            //Rotate hope by 90
+            hoopeNode.eulerAngles.x -= .pi / 2
+            
+            isHoopAdded = true
+            sceneView.scene.rootNode.addChildNode(hoopeNode)
+        }
     }
 }
